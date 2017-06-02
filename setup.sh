d965b428c21675f2eef6a6cf89865e614123c57e
Wen Zhang <zhangwen@cs.stanford.edu>
2017-06-02 19:26:42 +0000

    updated setup

--- setup.sh
+++ setup.sh
@@ -6,10 +6,11 @@ if [ "$EUID" -ne 0 ]
 fi
 
 echo "Installing dependencies..."
-apt-get install build-essential git debhelper autotools-dev dh-autoreconf iptables \
+apt-get update
+apt-get --yes --force-yes install build-essential git debhelper autotools-dev dh-autoreconf iptables \
 protobuf-compiler libprotobuf-dev pkg-config libssl-dev dnsmasq-base ssl-cert libxcb-present-dev \
 libcairo2-dev libpango1.0-dev iproute2 apache2-dev apache2-bin iptables dnsmasq-base gnuplot \
-iproute2 apache2-api-20120211 libwww-perl
+iproute2 apache2-api-20120211 libwww-perl python-pip
 
 pip install -r requirements.txt
 
